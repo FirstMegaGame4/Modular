a11aacb2d0795fcc484cf04f87e6d40010f357d1
FirstMegaGame4 <firstmegagame4@outlook.fr>
2022-04-02 20:30:38 +0200

    Add All Files

---
+++ src/main/java/fr/firstmegagame4/modular/Modular.java
@@ -0,0 +1,8 @@
+package fr.firstmegagame4.modular;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Modular implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
